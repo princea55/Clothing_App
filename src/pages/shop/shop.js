61c5ed426d2b17ef49940abdbc386d77049bac90
prince-suriya <princesuriya14@gmail.com>
2020-08-16 10:37:25 +0530

    added collection-preview and collection-item component

---
+++ src/pages/shop/shop.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import SHOP_DATA from './shopData';
+import CollectionsPreview from '../../components/preview-collection/collections-preview';
+
+export default class ShopPage extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+
+           collections: SHOP_DATA
+        }
+    }
+    render() {
+        const {collections} = this.state;
+        return (
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...othercollectionprops}) => (
+                        <CollectionsPreview key={id} {...othercollectionprops}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
